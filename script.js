5fc5102e681f8ae09162890e6e1ddb6e1ec25052
lucasaddisi <lucas.addisi@gmail.com>
2017-06-05 00:15:13 -0300

    Minor Modifications

---
+++ script.js
@@ -0,0 +1,13 @@
+var n1 = 0;
+document.write("This is an example");
+
+console.log("Esta funcion muestra salida en consola");
+alert("El valor final es: " + n1 );
+
+var example= {
+  marca: "Ferrari",
+  modelo: "458 Italia",
+  color: "Rojo"
+}
+
+var numeros = [1, 2, 3, 5];
