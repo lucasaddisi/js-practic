e05fbac445abea863d666ec24ef3f558ae079d35
lucasaddisi <lucas.addisi@gmail.com>
2017-06-06 16:40:29 -0300

    minnor changes

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 var n1 = 0;
-document.write("This is an example");
 
 console.log("Esta funcion muestra salida en consola");
-alert("El valor final es: " + n1 );
 
-var example= {
-  marca: "Ferrari",
-  modelo: "458 Italia",
-  color: "Rojo"
+var num = 50;
+
+var Auto= function(marca, modelo, color, maxKm){
+  this.marca = marca;
+  this.modelo = modelo;
+  this.color = color;
+  this.maxKm =  maxKm;
 }
 
-var numeros = [1, 2, 3, 5];
+var ferrari = new Auto("Ferrar", "458 Italia", "Rojo", 300);
+
+Auto.prototype.acelerar = function () {
+    for (i = 0; i <= maxKm; i += 1){
+      console.log(i);
+    }
+};
+console.log(ferrari);
+ferrari.acelerar;
